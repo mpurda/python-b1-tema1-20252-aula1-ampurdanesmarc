60edb0886f9cb83faf01b6aeca7872bb7654de48
mpurda <m.ampurdanes@gmail.com>
2026-04-07 15:55:09 +0200

    ej1a2

--- 1a/ej1a2.py
+++ 1a/ej1a2.py
@@ -42,9 +42,15 @@ Exemple:
 '''
 
 def sum_odd_numbers(list_numbers):
-    # Write here your code
-    pass
-
+    result = 0
+    for value in list_numbers:
+        if not isinstance(value, int):
+            raise ValueError("MENSAJE DE ERROR")
+        elif value < 0:
+            raise ValueError("MENSAJE DE ERROR")
+        elif value % 2 != 0:
+            result += value
+    return result
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # print(sum_odd_numbers([1, 2, 3, 4, 5, 10, 21, 100]))
